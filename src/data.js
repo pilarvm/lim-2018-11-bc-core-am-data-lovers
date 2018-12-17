72740d3af7c2c48149ac5b6cbcb927b54512aed3
Heidy Gerbi <heidyjoanagr@gmail.com>
2018-12-17 03:54:45 -0500

    estructura html, menu, slider, logica de las funciones de busqueda

--- src/data.js
+++ src/data.js
@@ -1,8 +1,45 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const example = () => {
+//   return 'example';
+// };
 
-window.example = example;
+// window.example = example;
+
+// //seleccionar pais
+// let selectCountry = (country) => {
+//   return WORLDBANK.country;
+//  };
+//  console.log(selectCountry("PER"));
+ 
+ 
+//  let dateRange = (country,dateInitial,dateEnd) => {
+//   //const country="PER";
+//   const positionId= 3;
+//   const arraicito=[];
+//  for( let i = dateInitial; i<=dateEnd; i++){
+//   arraicito[i] = WORLDBANK[country].indicators[positionId].data[i];
+//  }
+//  return arraicito;
+//  };
+//  console.table(dateRange("CHL",1990,2013));
+//  }
+
+//FILTRAR POR INDICADOR
+
+// const indi=WORLDBANK.PER.indicators;
+// const textindicatorCode='SL.TLF.PART.FE.ZS';
+// const iniDate=1990;
+// const lastDate=2010;
+
+
+// const selectIndicator = indi
+// .filter(indi => indi.indicatorCode === textindicatorCode);
+
+// console.log(selectIndicator);
+
+// const selectIndicator = indi
+// .filter(indi => indi.indicatorCode === textindicatorCode);
+
+// console.log(selectIndicator);
