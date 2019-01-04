3be6246594533c23a80689a9dbbde72727fa63a5
pjvarillas <pjvarillas@gmail.com>
2019-01-04 18:30:10 -0500

    reestructuracion de data y main no funcional

--- src/data.js
+++ src/data.js
@@ -1,161 +1,118 @@
 // variables globales 
-let dateOne = 0;
-let dateTwo = 0;
-
 /* global WORLDBANK:true */
 /* eslint no-undef: "error" */
 
-// Funciones para crear tablas dinámicas
-
-const addTableCaption = (caption) => {
-  let node = document.createElement("H3");
-  let textnode = document.createTextNode(caption + 'Años ' + dateOne + ' ' + dateTwo);
-  node.appendChild(textnode);
-  document.getElementById("caption").appendChild(node);
-}
-
-const newRowTableCountry = (country, docElemt) => {
-  let nameTable = document.getElementById(docElemt);
+/* *********** FUNCIONES PARA CREAR TABLAS DINÁMICAS *********** */
+// Función para pinta caption con rango de años
+// Función para conocer caption de indicators
+const indicatiorsCaption = (indicatorsArr, ind) => {
+  let indicatorNameArr = [];
+  indicatorNameArr = indicatorsArr.filter(populationElement => populationElement.indicatorCode === ind);
+  return indicatorNameArr;
+};
+// Función que retorna fila de paises seleccionados
+const newRowTableCountry = (country) => {
+  let nameTable = document.getElementById('table_result');
   let row = nameTable.insertRow(0);
   let countryArr = country.slice(0);
   countryArr.unshift('Año');
-  for (let i = 0; i < countryArr.length; i++) {
-    row.insertCell(i).innerHTML = countryArr[i];
-  }
+  for (let i = 0; i < countryArr.length; i++) row.insertCell(i).innerHTML = countryArr[i];
 };
-
-const newRowTableYear = (dateOne, dateTwo, docElemt) => {
-  let nameTable = document.getElementById(docElemt);
+// Función que retorna columna de años seleccionados
+const newRowTableYear = (dateOne, dateTwo) => {
+  let nameTable = document.getElementById('table_result');
   for (let i = dateTwo; i >= dateOne; i--) {
     let row = nameTable.insertRow(0 + 1);
     row.insertCell(0).innerHTML = i;
   }
 };
-
-const newRowTableYearProp = (goThroughRight, j, prop) => {
+// Función que retorna los resultdos segun pais y año seleccionado
+const newRowTableYearProp = (j, prop) => {
   let row = document.getElementsByTagName('tr')[j];
-  let x = row.insertCell(goThroughRight);
+  let x = row.insertCell(-1);
   x.innerHTML = prop;
 };
 
-// función para seleccionar multiples paises
-const searchMutipleCountry = () => {
-  const country = document.frm['country[]'];
-  const CountryArray = [];
-  for (let i = 0; i < country.length; i++) if (country[i].checked) CountryArray.push(country[i].value);
-  return CountryArray;
+
+
+/* *********** FUNCIONES ESPECIALIZADAS *********** */
+// Función con forEach 
+// const showForEach = (country, dateOne, dateTwo, ind) => {
+//   let matrix = [];
+//   let nameLimit = 0;
+//   country.forEach((element) => {
+//     let j = 0;
+//     let indicatorDateArr = [];
+//     let indicatorResult = [];
+//     let indicatorNameArr = [];
+//     const indicatorsArr = WORLDBANK[element].indicators;
+//     indicatorNameArr = indicatiorsCaption(indicatorsArr, ind);
+//     indicatorDateArr = indicatorNameArr[0].data;
+//     if (nameLimit === 0) addTableCaption(indicatorNameArr[0].indicatorName);
+//     nameLimit++;
+//     // console.log(indicatorDateArr);
+//     for (let prop in indicatorDateArr) {
+//       if (prop >= dateOne && prop <= dateTwo) { // prop hace referencia a la propiedad del objeto (en este caso años)
+//         let indDateArrTwoDec = 0;
+//         j++; // contador de rango de años (ejem: 2010 a 2012 recorre de 0 a 1 - 2 veces) - debe estar en la parte superior
+//         // condicional que combierte los "" string (espacios vacios) en 0 para evitar que toFixed genere error
+//         if (typeof(indicatorDateArr[prop]) === 'number') indDateArrTwoDec = indicatorDateArr[prop].toFixed(2);
+//         else indDateArrTwoDec = '';
+//         newRowTableYearProp(j, indDateArrTwoDec);
+//         indicatorResult.push(indDateArrTwoDec);
+//       }
+//     }
+//     // console.log(`esto es push de indicatorResult ${indicatorResult}`);
+//     matrix.push(indicatorResult);
+//   //   console.log(`esto es matrix (en proceso)  ${matrix}`);
+  // });
+  // console.log(`esto es matrix (fuera de for): ${matrix}`);
+  // console.log (matrix);
+// };
+// FILTRAR POR INDICADOR
+const showResult = () => {
+  // let country = searchMutipleCountry();
+  // searchRangeYear();
+  // newRowTableCountry(country);
+  // newRowTableYear(dateOne, dateTwo);
+  // showForEach(country, dateOne, dateTwo);
 };
 
-// función para seleccionar una rango de fechas
-const searchRangeYear = () => {
-  dateOne = document.frm['date-one'].value;
-  dateTwo = document.frm['date-two'].value;
-  if (dateOne.value >= dateTwo.value) return alert('Rango de fecha inválido');
-  else return dateOne, dateTwo;
-};
-
-// función para conocer indicador deseado
-const searchIndicator = () => {
-  let indicat = document.frm['category[]'];
-
-  let IndicatorString = '';
-  for (let i = 0; i < indicat.length; i++) {
-    if (indicat[i].checked) {
-      IndicatorString = indicat[i].value;
-    }
-  }
-  return IndicatorString;
-};
-
-
+// Función para promedio EN PROCESO
 const promResult = (array) => {
   let sum = array.reduce((previous, current) => current += previous);
   let avg = sum / array.length;
   console.log(avg);
-
-}
-// función para conocer genero seleccionado
-const searchSex = () => {
-  let sexArr = document.frm['sex[]'];
-
-  let sexString = '';
-  for (let i = 0; i < sexArr.length; i++) {
-    if (sexArr[i].checked) {
-      sexString = sexArr[i].value;
-    }
-  }
-  return sexString;
+};
+// Función para ordenar Ascendentemente
+const orderAsc = () => {
+  let matriz = showResult();
+  alert(matriz);
+  // una vez que rotorne ma matriz adecuada se ordenara de modos ASC
 };
 
-// función para identificar indicador específico (según sexo)
-const indicator = () => {
-  let indicator = '';
-  const IndicatorString = searchIndicator();
-  const sexString = searchSex();
-  if (IndicatorString === 'SL.TLF.BASC.ZS' && sexString === 'FE') indicator = 'SL.TLF.BASC.FE.ZS';
-  else if (IndicatorString === 'SL.TLF.BASC.ZS' && sexString === 'MA') indicator = 'SL.TLF.BASC.MA.ZS';
-  else if (IndicatorString === 'SL.TLF.INTM.ZS' && sexString === 'FE') indicator = 'SL.TLF.INTM.FE.ZS';
-  else if (IndicatorString === 'SL.TLF.INTM.ZS' && sexString === 'MA') indicator = 'SL.TLF.INTM.MA.ZS';
-  else if (IndicatorString === 'SL.TLF.ADVN.ZS' && sexString === 'FE') indicator = 'SL.TLF.ADVN.FE.ZS';
-  else if (IndicatorString === 'SL.TLF.ADVN.ZS' && sexString === 'MA') indicator = 'SL.TLF.ADVN.MA.ZS';
-  else if (IndicatorString === 'SL.TLF.ACTI.ZS' && sexString === 'FE') indicator = 'SL.TLF.ACTI.FE.ZS';
-  else if (IndicatorString === 'SL.TLF.ACTI.ZS' && sexString === 'MA') indicator = 'SL.TLF.ACTI.MA.ZS';
-  return indicator;
+// Función para ordenar Ascendentemente
+const orderDesc = () => {
+  let matriz = showResult();
+  alert(matriz);
+  // una vez que rotorne ma matriz adecuada se ordenara de modos DESC
 };
-
-// FILTRAR POR INDICADOR
-const showResult = () => {
-  Reset(); 
-  let matrix = [];
-  let contador = 0;
-  searchRangeYear();
-  let country = searchMutipleCountry();
-  newRowTableCountry(country, 'table_result');
-  newRowTableYear(dateOne, dateTwo, 'table_result');
-
-  country.forEach((element) => {
-    const indicatorsArr = WORLDBANK[element].indicators;
-    let goThroughRight = 0;
-    let j = 0;
-    let indicatorResult = [];
-    goThroughRight--;
-    let indicatorNameArr = [];
-
-    indicatorNameArr = indicatorsArr.filter(populationElement => populationElement.indicatorCode === indicator());
-    if (contador == 0) addTableCaption(indicatorNameArr[0].indicatorName);
-    contador++;
-    let indicatorDateArr = [];
-    indicatorDateArr = indicatorNameArr[0].data;
-
-    for (let prop in indicatorDateArr) {
-
-      if (prop >= dateOne && prop <= dateTwo) {
-        j++;
-        newRowTableYearProp(goThroughRight, j, indicatorDateArr[prop].toFixed(2));
-        indicatorResult.push(indicatorDateArr[prop].toFixed(2));
-
-      }
-    }
-    console.log(indicatorResult);
-    promResult(indicatorResult);
-    matrix.push(indicatorResult);
-    console.log(matrix);
-  });
+const frequentIndicator = (ind) => {
+  let country = ['PER', 'CHL', 'MEX', 'BRA'];
+  const indicator = ind;
+  newRowTableCountry(country);
+  newRowTableYear(2017, 2017);
+  showForEach(country, 2017, 2017, indicator);
 };
-
-const Reset = () => {
-  document.getElementById('table_result').innerHTML = '';
-  document.getElementById('caption').innerHTML = '';
-};
-
 window.worldbank = {
-  newRowTableCountry,
-  newRowTableYear,
-  newRowTableYearProp,
-  searchMutipleCountry,
-  searchRangeYear,
-  searchIndicator,
-  searchSex,
-  indicator,
+  // newRowTableCountry,
+  // newRowTableYear,
+  // newRowTableYearProp,
+  // promResult,
+  // orderAsc,
+  // orderDesc,
+  // frequentIndicator,
+  // indicatiorsCaption,
+  indicatiorsCaption,
   showResult
 };
--- src/main.js
+++ src/main.js
@@ -1,7 +1,166 @@
-﻿// btn-submit
-const btnSubmit = document.getElementById('btn-submit');
-btnSubmit.addEventListener('click', () => {
-/* global worldbank:true */
+﻿/* global worldbank:true */
 /* eslint no-undef: "error" */
-worldbank.showResult();
+// let dateOne = 0;
+// let dateTwo = 0;
+
+
+// btn-submit
+const btnSubmit = document.getElementById('btn-submit');
+let btnIndex = document.getElementById('btn-index');
+let btnSearch = document.getElementById('btn-search');
+
+btnSubmit.addEventListener('click', () => {
+  searchRangeYear();
+  searchMutipleCountry();
+  indicator();
+
+  worldbank.showResult();
+  
+console.log(searchMutipleCountry());
+console.log(indicator());
+console.log(searchRangeYear());
+  // addTableCaption();
 });
+
+let addTableCaption = (indi) => {
+  const dateRange = searchRangeYear();
+  indicator(indi);
+  
+  let caption = worldbank.indicatiorsCaption();
+  const node = document.createElement('H3');              
+  const textnode = document.createTextNode(`${caption}, del ${dateRange[0]} al ${dateRange[1]}`);         
+  node.appendChild(textnode);                              
+  document.getElementById('caption').appendChild(node); 
+};
+
+
+
+/* *********** FUNCIONES PARA MENÚ DE BÚSQUEDA *********** */
+// función para seleccionar multiples paises
+const searchMutipleCountry = () => {
+  const country = document.frm['country[]'];
+  const CountryArray = [];
+  for (let i = 0; i < country.length; i++) 
+    if (country[i].checked) CountryArray.push(country[i].value);
+    // else return alert('Debe selecionar al menos 1 país');
+  return CountryArray;
+};
+// función para seleccionar una rango de fechas
+const searchRangeYear = () => {
+  const dateRange = [document.frm['date-one'].value, document.frm['date-two'].value];
+  if (dateRange[0].value >= dateRange[1].value) return alert('Rango de fecha inválido');
+  else return dateRange;
+};
+// función para conocer indicador deseado
+const searchIndicator = () => {
+  let indicat = document.frm['category[]'];
+  let IndicatorString = '';
+  for (let i = 0; i < indicat.length; i++) 
+    if (indicat[i].checked) IndicatorString = indicat[i].value;
+    // else return 'Debe selecionar 1 categoría';
+  return IndicatorString;
+};
+// función para conocer genero seleccionado
+const searchSex = () => {
+  let sexArr = document.frm['sex[]'];
+  let sexString = '';
+  for (let i = 0; i < sexArr.length; i++)
+    if (sexArr[i].checked) sexString = sexArr[i].value;
+    // else return 'Debe selecionar 1 genero';
+  return sexString;
+};
+// función para identificar indicador específico (según sexo)
+const indicator = (ind) => {
+  let indicator = '';
+  const IndicatorString = searchIndicator();
+  const sexString = searchSex();
+  if (IndicatorString === 'SL.TLF.BASC.ZS' && sexString === 'FE') indicator = 'SL.TLF.BASC.FE.ZS';
+  else if (IndicatorString === 'SL.TLF.BASC.ZS' && sexString === 'MA') indicator = 'SL.TLF.BASC.MA.ZS';
+  else if (IndicatorString === 'SL.TLF.INTM.ZS' && sexString === 'FE') indicator = 'SL.TLF.INTM.FE.ZS';
+  else if (IndicatorString === 'SL.TLF.INTM.ZS' && sexString === 'MA') indicator = 'SL.TLF.INTM.MA.ZS';
+  else if (IndicatorString === 'SL.TLF.ADVN.ZS' && sexString === 'FE') indicator = 'SL.TLF.ADVN.FE.ZS';
+  else if (IndicatorString === 'SL.TLF.ADVN.ZS' && sexString === 'MA') indicator = 'SL.TLF.ADVN.MA.ZS';
+  else if (IndicatorString === 'SL.TLF.ACTI.ZS' && sexString === 'FE') indicator = 'SL.TLF.ACTI.FE.ZS';
+  else if (IndicatorString === 'SL.TLF.ACTI.ZS' && sexString === 'MA') indicator = 'SL.TLF.ACTI.MA.ZS';
+  else if (ind === 'SH') indicator = 'SH.ANM.ALLW.ZS';
+  else if (ind === 'SG') indicator = 'SG.VAW.REAS.ZS';
+  else if (ind === 'SP') indicator = 'SP.POP.TOTL.FE.ZS';
+  else if (ind === 'IC') indicator = 'IC.REG.DURS.FE';
+  else if (ind === 'ICF') indicator = 'IC.FRM.FEMM.ZS';
+  else if (ind === 'COV') indicator = 'per_allsp.cov_pop';
+  return indicator;
+};
+
+const hidenDiv = (div1, div2) => {
+  document.getElementById(div1).style.display = 'block';
+  document.getElementById(div2).style.display = 'none';
+};
+
+btnIndex.addEventListener('click', () => {
+  hidenDiv('index', 'search');
+});
+
+btnSearch.addEventListener('click', () => {
+  hidenDiv('search', 'index');
+});
+
+// Creamos un array vacio
+let ElementClick = '';
+// Capturamos el click y lo pasamos a una funcion
+document.onclick = captureClick;
+function captureClick(element) {
+  // Funcion para capturar el click del raton
+  let click;
+  if (element !== null) {
+    // Si ha hecho click sobre un destino, lo leemos
+    click = element.target;
+  }
+  // Añadimos el elemento al array de elementos
+  ElementClick = click;
+  // Una prueba con salida en consola
+  switch(ElementClick.id) {
+    case 'SH':
+    addTableCaption(ElementClick.id);
+     break;
+    case 'SG':
+    indicator('SG');
+    break;
+    case 'SP':
+    indicator('SP');
+    break;
+    case 'IC':
+    indicator('IC');    
+    break;
+    case 'ICF':
+    indicator('ICF');
+    break;
+    case 'COV':
+    indicator('COV');
+    break;
+    // default:
+    //   alert('selección invalida');
+  }
+}
+
+// // btn-order-asc
+// const btnOrderAsc = document.getElementById('order-asc');
+// btnOrderAsc.addEventListener('click', () => {
+//   worldbank.orderAsc();
+// });
+
+// // btn-order-desc
+// const btnOrderDesc = document.getElementById('order-desc');
+// btnOrderAsc.addEventListener('click', () => {
+//   worldbank.orderDesc();
+// });
+
+
+
+// // Función para pinta caption con rango de años
+// const addTableCaption = (caption) => {
+//   worldbank.searchRangeYear();
+//   const node = document.createElement('H3');              
+//   const textnode = document.createTextNode(`${caption}, del ${dateOne} al ${dateTwo}`);         
+//   node.appendChild(textnode);                              
+//   document.getElementById('caption').appendChild(node); 
+// };
